7f2a7bb1585e4207fda24a9624348748676f41d4
Krinsi Kayada <81806503+Kayadakrinsi@users.noreply.github.com>
2024-01-08 16:51:29 +0530

    API UPLOADED

---
+++ API/2. Basics of C#/OOP/OOP/Encapsulation.cs
@@ -0,0 +1,184 @@
+﻿// Public class
+public class PublicClass
+{
+    #region Private Members
+
+    string _Name { get; set; }
+
+    #endregion
+
+
+    #region Public Members
+
+    public static int age = 20;
+    public string Description { get; set; }
+
+    #endregion
+
+
+    #region Constructors
+
+    public PublicClass() { }
+
+    public PublicClass(string name, string description)
+    {
+        this._Name = name;
+        this.Description = description;
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    // Displays information of about 
+    public void About()
+    {
+        Console.WriteLine("Name : ", this._Name);
+        Console.WriteLine("Age : ", PublicClass.age);
+        Console.WriteLine("About : ", this.Description);
+    }
+
+    #endregion
+
+}
+
+// Child class of public class
+public class ChildPublic : PublicClass
+{
+    #region Public Members
+
+    public string City { get; set; }
+    public ChildPublic() { }
+
+    #endregion
+
+    #region Public Methods
+
+    public ChildPublic(string name, string description, string city)
+    {
+        _Name = name; // Private member of public class is not accessible here
+        Description = description;
+        City = city;
+    }
+
+    #endregion
+
+}
+
+// Private class
+class PrivateClass
+{
+    #region Private Members
+
+    string _Name { get; set; }
+
+    #endregion
+
+
+    #region Public Members
+
+    public static int age = 20;
+    public string Description { get; set; }
+
+    #endregion
+
+    #region Constructors
+
+    public PrivateClass() { }
+
+    public PrivateClass(string name, string description)
+    {
+        _Name = name;
+        Description = description;
+    }
+
+    #endregion
+
+
+    #region Private Methods 
+
+    // Displays information of about 
+    void About()
+    {
+        Console.WriteLine("Name : ", _Name);
+        Console.WriteLine("Age : ", age);
+        Console.WriteLine("About : ", Description);
+    }
+
+    #endregion
+
+
+    #region Public Methods
+
+    // Returns private member _Name publicly
+    public string GetName()
+    {
+        return _Name;
+    }
+
+    // Returns private member _Name in protected mode
+    //protected string GetName()
+    //{
+    //    return Name;
+    //}
+
+    #endregion
+
+}
+
+// Child class of Private class
+public class ChildPrivate : PrivateClass // Not able to access Private class due to encapsulation
+{
+    public string _Name { get; set; }
+}
+
+public static class StaticClass //static classes are sealed hence not able to inheritance
+{
+    #region Private Methods
+
+    static int _age = 22;
+
+    #endregion
+
+
+    #region Public Members
+
+    public static string name = "Krinsi";
+
+    #endregion
+
+
+    #region Public Methods
+
+    // Displays information of about 
+    public static void About()
+    {
+        Console.WriteLine("Name : " + name);
+        Console.WriteLine("Age : " + _age);
+    }
+
+    #endregion
+
+}
+
+public class ProgramEncapsulation
+{
+    public static void Main()
+    {
+        Console.WriteLine("Age from static class : " + StaticClass._age);
+        Console.WriteLine("Name from static class : " + StaticClass.name);
+        StaticClass.About();
+        Console.WriteLine();
+
+        Console.WriteLine("Age from public class : " + PublicClass.age);
+        PublicClass publicClass = new PublicClass("Krinsi", "Hello, I am student");
+        publicClass.About();
+
+        Console.WriteLine("Age from private class : " + PrivateClass.age);
+        PrivateClass privateClass = new PrivateClass("Kayada", "Hello, I am fine");
+        privateClass.About();
+        Console.WriteLine("Name from private class : " + privateClass._Name);
+        string name = privateClass.GetName();
+        Console.WriteLine("Name from private class : " + name);
+    }
+}
---
+++ API/2. Basics of C#/OOP/OOP/Inheritance.cs
@@ -0,0 +1,203 @@
+﻿using System.Diagnostics.Contracts;
+
+// Base Class Animal (implemets herarical inheritance)
+public class Animal
+{
+    #region Public Members
+
+    public static int noOfFeet = 4;
+    public string? animalType;
+
+    #endregion
+
+
+    #region Public Methods
+
+    // Displays sound of animal
+    public void Sound()
+    {
+        Console.WriteLine("Animal sound...");
+    }
+
+    #endregion
+}
+
+// Class Dog inherits class Animal
+public class Dog : Animal
+{
+    #region Public Members
+
+    public string bark;
+
+    #endregion
+
+
+    #region Constructors
+
+    public Dog(string animalType, string bark) : base()
+    {
+        this.animalType = animalType;
+        this.bark = bark;
+    }
+
+    #endregion
+
+
+    #region Public Methods
+
+    // Overrides sound() method and displays sound of Dog
+    public void Sound()
+    {
+        Console.WriteLine("Dog sound : " + this.bark);
+    }
+
+    #endregion
+
+}
+
+// Class Huskey inherits class Dog (MultiLevel inheritance)
+public class Huskey : Dog
+{
+    #region Public Members
+
+    public static string color = "White and grey";
+
+    #endregion
+
+
+    #region Constructors
+
+    public Huskey(string animalType, string bark) : base(animalType,bark)
+    {
+        this.animalType = animalType;
+        this.bark = bark;
+    }
+
+    #endregion
+
+
+    #region Public Methods
+
+    // Overrides sound() method and displays sound of Huskey
+    public void Sound()
+    {
+        Console.WriteLine("Huskey sound : " + this.bark);
+    }
+
+    // Displays color of Huskey
+    public void ColorDisplay()
+    {
+        Console.WriteLine("The color of Huskey is : " + Huskey.color);
+    }
+
+    #endregion
+
+}
+
+// Class Cat inherits class Animal
+public class Cat : Animal
+{
+    #region Public Members
+
+    public string purr;
+
+    #endregion
+
+
+    #region Constructors
+
+    public Cat(string animalType, string purr)
+    {
+        this.animalType = animalType;
+        this.purr = purr;
+    }
+
+    #endregion
+
+
+    #region Public Methods
+
+    // Overrides sound() method and displays sound of Cat
+    public void Sound()
+    {
+        Console.WriteLine("Cat sound : " + this.purr);
+    }
+
+    #endregion
+
+}
+
+// Sealed class
+sealed class Cars
+{
+    #region Public Members
+
+    public static int noOfTyres = 4;
+    public string Model { get; set; }
+    public int topSpeed;
+    public string color;
+
+    #endregion
+
+
+    #region Constructors
+
+    public Cars() { }
+    public Cars(string model, int topSpeed, string color)
+    {
+        Model = model;
+        this.topSpeed = topSpeed;
+        this.color = color;
+    }
+
+    #endregion
+
+
+    #region Public Methods
+
+    // Displays details of car
+    public void Details()
+    {
+        Console.WriteLine("Car model : " + Model);
+        Console.WriteLine("Top speed : " + topSpeed);
+        Console.WriteLine("Color : " + color);
+    }
+
+    #endregion
+}
+
+// Try to inherit sealed class
+//public class mustang : cars
+//{
+
+//}
+
+public class ProgramInheritance
+{
+    public static void Main()
+    {
+        Console.WriteLine("Static property number of feet is " + Animal.noOfFeet);
+        Animal animal = new Animal();
+        animal.Sound();
+        Console.WriteLine();
+
+        Console.WriteLine("Static property number of feet is " + Dog.noOfFeet);
+        Dog doggo = new Dog("dog", "Bhaw Bhaw...");
+        doggo.Sound();
+        Console.WriteLine();
+
+        Console.WriteLine("Static property number of feet is " + Cat.noOfFeet);
+        Cat milli = new Cat("Cat", "Meow Meow...");
+        milli.Sound();
+        Console.WriteLine();
+
+        Console.WriteLine("Static property number of feet is " + Huskey.noOfFeet);
+        Huskey huskey = new Huskey("Dog", "Howww Howww...");
+        huskey.Sound();
+        huskey.ColorDisplay();
+        Console.WriteLine();
+
+        Cars cars = new Cars("Bugatti", 450, "Black");
+        cars.Details();
+    }
+}
