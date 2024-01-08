8fa5470abe550c3ce05e8ded697255c286713053
Krinsi Kayada <81806503+Kayadakrinsi@users.noreply.github.com>
2024-01-08 16:43:34 +0530

    API UPLOADED

---
+++ API/2. Basics of C#/Collections/Collections/ListKeyValue.cs
@@ -0,0 +1,14 @@
+﻿
+public class Element
+{
+    #region Public Members
+    public string Symbol { get; set; }
+    public string Name { get; set; }
+    public int AtomicNumber { get; set; }
+
+    #endregion
+
+}
+
+
+
